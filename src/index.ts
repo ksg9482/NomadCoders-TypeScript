e3ebeeef07ca9504f4cb76661b8b5cf0e57f8ef9
ksg9482 <ksg930523@gmail.com>
2022-02-22 00:25:12 +0900

    part1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from "crypto-js"
+
 class Block {
     public index: number
     public hash: string
     public previousHash: string
     public data: string
     public timeStamp: number
+//class 바깥에서 바로 클래스 내부의 메서드에 접근 ,사용하려면 static을 써야한다
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        data: string,
+        timeStamp: number
+    ): string => CryptoJS.SHA256(index + previousHash + data + timeStamp).toString()
+
     constructor(
         index: number,
         hash: string,
@@ -21,7 +31,14 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "123123123", "", "hello", 123456)
 
-let blockChain:[Block] = [genesisBlock]
+let blockChain:Block[] = [genesisBlock]
+
+const getBlockChain = (): Block[]/*return은 Block타입 배열*/ => blockChain
+
+//블록체인 안에서 가장 최근 것
+const getLatestBlockChain = (): Block/*return은 Block타입*/ => blockChain[blockChain.length - 1]
+
+const getNewTimeStamp = (): number => Math.round(new Date().getDate() / 1000)
 
 console.log(blockChain)
 
